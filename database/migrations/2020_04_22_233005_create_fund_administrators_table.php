54dcb198ccefa910389bd5c1ef2e772348e1a5cf
Claudio Briones <brionescl@gmail.com>
2020-04-30 16:46:36 -0400

    Invoke seeder from migration

--- database/migrations/2020_04_22_233005_create_fund_administrators_table.php
+++ database/migrations/2020_04_22_233005_create_fund_administrators_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 // @codingStandardsIgnoreLine
@@ -19,6 +20,10 @@ class CreateFundAdministratorsTable extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        Artisan::call('db:seed', [
+            '--class' => FundAdministratorSeeder::class,
+        ]);
     }
 
     /**
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            FundAdministratorSeeder::class
+            // OtherSeeder::class
         ]);
     }
 }
