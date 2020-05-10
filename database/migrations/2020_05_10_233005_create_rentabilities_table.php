5efa65b2ba1824f16dd4cb4a04fa0dfb701d963c
Claudio Briones <brionescl@gmail.com>
2020-05-10 16:46:41 -0400

    Add rentabilities table migration

---
+++ database/migrations/2020_05_10_233005_create_rentabilities_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+// @codingStandardsIgnoreLine
+class CreateRentabilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentabilities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('fund_administrator_id');
+            $table->string('investment_fund');
+            $table->date('date');
+            $table->float('rentability');
+            $table->timestamps();
+            $table->foreign('fund_administrator_id')->references('id')->on('fund_administrators');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentabilities');
+    }
+}
